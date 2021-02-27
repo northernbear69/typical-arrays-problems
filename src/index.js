dc909bdc35a1ffd47da9a8335dc8d7164978d24b
northernbear69 <northernbear69webdev@gmail.com>
2021-02-27 21:45:10 +0300

    feat: add func

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-
 exports.min = function min (array) {
-  return 0;
+  if(!array || array.length === 0) return 0;
+  let result = array[0];
+  array.forEach(element => {
+    if(element < result) result = element;    
+  });
+  return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length === 0) return 0;
+  let result = array[0];
+  array.forEach(element => {
+    if(element > result) result = element;    
+  });
+  return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.length === 0) return 0;
+  let result = 0;
+  array.forEach(element => {
+    result += element;
+  })
+  return result / array.length;
 }
